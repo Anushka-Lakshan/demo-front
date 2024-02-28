3a772330ac0b115b93816602aec0cf4085431806
Anushka lakshan <106550627+Anushka-Lakshan@users.noreply.github.com>
2024-02-28 23:29:54 +0530

    filter button optimized

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,8 @@
 const container = document.querySelector(".container-nav");
 const primaryNav = document.querySelector(".nav__list");
-const toggleButton = document.querySelector(".nav-toggle");
+const toggleButton22 = document.querySelector(".nav-toggle");
 
-toggleButton.addEventListener("click", () => {
+toggleButton22.addEventListener("click", () => {
     const isExpanded = primaryNav.getAttribute("aria-expanded");
     primaryNav.setAttribute(
         "aria-expanded",
@@ -11,7 +11,7 @@ toggleButton.addEventListener("click", () => {
 });
 
 container.addEventListener("click", (e) => {
-    if (!primaryNav.contains(e.target) && !toggleButton.contains(e.target)) {
+    if (!primaryNav.contains(e.target) && !toggleButton22.contains(e.target)) {
         primaryNav.setAttribute("aria-expanded", "false");
     }
 });
